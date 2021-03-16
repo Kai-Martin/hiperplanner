388d41eaa37f95c88a8493400775acbb423cbfc2
Evan Sharp <ego@evansharp.ca>
2021-03-16 15:40:03 -0700

    Update Email.php

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
 	/**
 	 * @var string
 	 */
-	public $fromEmail;
+	public $fromEmail = 'kai.martin@coastmountainacademy.ca';
 
 	/**
 	 * @var string
 	 */
-	public $fromName;
+	public $fromName = 'Kai at Hiperplanner';
 
 	/**
 	 * @var string
