71b63c4cd65a37b1f79d34e1e67419891aa2ae2a
Evan Sharp <ego@evansharp.ca>
2021-03-16 15:11:52 -0700

    Update Autoload.php

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -42,7 +42,8 @@ class Autoload extends AutoloadConfig
 	public $psr4 = [
 		APP_NAMESPACE => APPPATH, // For custom app namespace
 		'Config'      => APPPATH . 'Config',
-		'Auth'		  => APPPATH . 'ThirdParty/Auth',
+		'App'           => APPPATH,
+		'Auth'		  => APPPATH . 'ThirdParty/Auth'
 	];
 
 	/**
