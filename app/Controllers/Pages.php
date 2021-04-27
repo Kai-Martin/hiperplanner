1052e8cee941f3e4dc1c22dd1eeb7c35c4e931e6
Kai-Martin <57156876+Kai-Martin@users.noreply.github.com>
2021-04-27 14:52:50 -0700

    Update Pages.php

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -5,7 +5,7 @@ class Pages extends BaseController
 {
 	public function index()
 	{
-		echo view('templates/header', $data);
+		echo view('templates/header');
 		echo view('pages/home');
 		echo view('templates/footer');
 	}
